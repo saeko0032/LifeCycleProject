46caf3b97577a6246175ad697e71337e6097eec5
Saeko Fukui <saeko0032@gmail.com>
2017-07-07 10:53:23 -0700

    chnged back to screen1 -> go to web page

--- app/src/main/java/com/example/ciccc_cirac/lifecycleproject/IntentexampleB.java
+++ app/src/main/java/com/example/ciccc_cirac/lifecycleproject/IntentexampleB.java
@@ -1,6 +1,7 @@
 package com.example.ciccc_cirac.lifecycleproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 /**
  * Created by Saeko Fukui on 2017-07-06.
+ * Updated by SF on 2017-07-07.
  */
 
 public class IntentexampleB extends AppCompatActivity {
@@ -27,8 +29,12 @@ public class IntentexampleB extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(IntentexampleB.this,IntentexampleA.class);
+                String url = "http://www.stackoverflow.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
                 startActivity(i);
+                //  Intent i = new Intent(IntentexampleB.this,IntentexampleA.class);
+                //  startActivity(i);
             }
         });
     }
