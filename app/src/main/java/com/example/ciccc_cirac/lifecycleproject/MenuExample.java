200569e364251dac37cabdef89a5e009652652f2
Saeko Fukui <saeko0032@gmail.com>
2017-07-11 13:23:28 -0700

    changed for add price and sending email function

--- app/src/main/java/com/example/ciccc_cirac/lifecycleproject/MenuExample.java
+++ app/src/main/java/com/example/ciccc_cirac/lifecycleproject/MenuExample.java
@@ -4,8 +4,11 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by CICCC-CIRAC on 2017-07-11.
@@ -26,5 +29,14 @@ public class MenuExample extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.menutext, menuItems);
         ListView listView = (ListView) findViewById(R.id.list_menu);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // text on which you are clicking
+                String clickedPosition = Integer.toString(position);
+                Toast t = Toast.makeText(getApplicationContext(),clickedPosition,Toast.LENGTH_LONG);
+                t.show();
+            }
+        });
     }
 }
