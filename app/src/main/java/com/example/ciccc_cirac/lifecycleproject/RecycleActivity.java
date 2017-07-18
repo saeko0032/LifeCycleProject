f064a456d54a9e2624d33f45141dd25d717ecfbc
Saeko Fukui <saeko0032@gmail.com>
2017-07-18 10:28:00 -0700

    add menu for recyclerview

--- app/src/main/java/com/example/ciccc_cirac/lifecycleproject/RecycleActivity.java
+++ app/src/main/java/com/example/ciccc_cirac/lifecycleproject/RecycleActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by saeko on 7/17/2017.
@@ -24,5 +26,28 @@ public class RecycleActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linaeraMng);
         adapter = new RecyclerAdapter(COUNTER);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.refresh_example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings) {
+            return true;
+
+        } else if (id == R.id.action_refresh) {
+            // Refresh adapter
+            // should create adapter again!!
+            adapter = new RecyclerAdapter(COUNTER);
+            recyclerView.setAdapter(adapter);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
